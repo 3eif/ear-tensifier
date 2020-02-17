3e54ddbc89e934249c8d00a1e54aa457ba0fdbdb
Tetra <seifaziz10@gmail.com>
2020-02-17 09:31:56 -0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,7 +6,6 @@ module.exports = {
     "bassboost": "200",
     "earrape": "500",
     "normal": "100",
-    "testing": "true",
     "avatar": "https://cdn.discordapp.com/attachments/673368062895980574/675948705039515678/cda.png",
     "devs": "275831434772742144",
 };
