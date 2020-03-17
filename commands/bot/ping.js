02f69dbec6b3b9a2fe302ccea841781c4530a2f1
Tetra <seifaziz10@gmail.com>
2020-03-17 08:10:15 -0700

    Reload command fix, ping command update, delete restart command

--- commands/bot/ping.js
+++ commands/bot/ping.js
@@ -5,12 +5,6 @@ module.exports = {
     description: "Bot's latency",
     async execute(client, message, args) {
         const msg = await message.channel.send(`${client.emojiList.loading} Pinging...`);
-
-        const embed = new Discord.MessageEmbed()
-            .setAuthor("Pong!", client.settings.avatar)
-            .setDescription(`Latency \`${msg.createdTimestamp - message.createdTimestamp}ms\`. API Latency \`${Math.round(client.ws.ping)}ms\``)
-            .setColor(client.colors.main)
-            .setTimestamp();
-        msg.edit("", embed);
+        return msg.edit(`Pong! (Latency: ${msg.createdTimestamp - message.createdTimestamp}ms. API Latency: ${Math.round(client.ws.ping)}ms.)`)
     },
 };
--- commands/dev/reload.js
+++ commands/dev/reload.js
@@ -2,13 +2,14 @@ module.exports = {
 	name: 'reload',
 	description: 'Reloads a command',
     args: true,
-	usage: "<command>",
+	usage: "<category> <command>",
 	permissions: "dev",
 	async execute(client, message, args) {
 
-        if(message.author.id !== client.settings.devs) return;
+		if(message.author.id !== client.settings.devs) return;
+		if(!args[1]) return message.channel.send(`Please provide a command.`)
 
-		const commandName = args[0].toLowerCase();
+		const commandName = args[1].toLowerCase();
 		const command = message.client.commands.get(commandName)
 			|| message.client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
 
@@ -16,10 +17,10 @@ module.exports = {
 			return message.channel.send(`There is no command with name or alias \`${commandName}\`, ${message.author}!`);
 		}
 
-		delete require.cache[require.resolve(`./${commandName}.js`)];
+		delete require.cache[require.resolve(`../${args[0]}/${commandName}.js`)];
 
 		try {
-			const newCommand = require(`./${commandName}.js`);
+			const newCommand = require(`../${args[0]}/${commandName}.js`);
 			message.client.commands.set(newCommand.name, newCommand);
 		} catch (error) {
 			console.log(error);
