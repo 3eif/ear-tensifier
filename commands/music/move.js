1567277d6f513915e652bca831ced0752d7ad46f
Tetra <seifaziz10@gmail.com>
2020-03-19 10:35:46 -0700

    Move command

--- commands/music/move.js
+++ commands/music/move.js
@@ -2,7 +2,7 @@ module.exports = {
     name: "move",
     description: "Moves a song to another location in the queue.",
     args: true,
-    usage: "<song position>",
+    usage: "<old position> <new position>",
     cooldown: '10',
     async execute(client, message, args) {
         const voiceChannel = message.member.voice.channel;
@@ -14,13 +14,21 @@ module.exports = {
 
         if(!player) return client.responses('noSongsPlaying', message)
 
-        if(args[0] == 0) return message.channel.send(`Cannot skip to a song that is already playing. To skip the current playing song type: \`${client.settings.prefix}skip\``)
+        if(args[0] == 0) return message.channel.send(`Cannot move a song that is already playing. To skip the current playing song type: \`${client.settings.prefix}skip\``)
         if((args[0] > player.queue.size) || (args[0] && !player.queue[args[0]])) return message.channel.send(`Song not found.`)
 
-        const { title } = player.queue[args[0]];
-        if(args[0] == 1) player.stop(); 
-        player.queue.splice(1, args[0]-1);
-        player.stop();
-        return message.channel.send(`Skipped to **${title}**.`)
+        if(!args[1]) {
+            let song = player.queue[args[0]];
+            player.queue.splice(args[0], 1)
+            player.queue.splice(1, 0, song);
+            return message.channel.send(`Moved **${song.title}** to the beginning of the queue.`)
+        } else if(args[1]){
+            if(args[1] == 0) return message.channel.send(`Cannot move a song that is already playing. To skip the current playing song type: \`${client.settings.prefix}skip\``)
+            if((args[1] > player.queue.size) || (args[1] && !player.queue[args[1]])) return message.channel.send(`Song not found.`)
+            let song = player.queue[args[0]];
+            player.queue.splice(args[0], 1)
+            player.queue.splice(args[1], 0, song);
+            return message.channel.send(`Moved **${song.title}** to the position ${args[1]}.`)
+        }
     }
 }
