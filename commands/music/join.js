466c893a11b8a37014c8314679778e10f3b11ddd
Tetra <seifaziz10@gmail.com>
2020-03-19 21:15:00 -0700

    Join command bug fix

--- commands/music/join.js
+++ commands/music/join.js
@@ -9,7 +9,12 @@ module.exports = {
 		const permissions = voiceChannel.permissionsFor(client.user);
 		if(!permissions.has('CONNECT')) return client.responses('noPermissionConnect', message);
 
-		voiceChannel.join();
+		// eslint-disable-next-line no-unused-vars
+		const player = client.music.players.spawn({
+			guild: message.guild,
+			textChannel: message.channel,
+			voiceChannel: voiceChannel,
+		});
 		return message.channel.send(`Joined ${client.emojiList.voice}**${voiceChannel.name}**`);
 	},
 };
