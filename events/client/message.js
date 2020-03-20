7d7de04aa9d4fdac7fa0703ce84bed41a7561b9e
Tetra <seifaziz10@gmail.com>
2020-03-20 05:42:09 -0700

    Fixed argument bug

--- events/client/message.js
+++ events/client/message.js
@@ -185,6 +185,13 @@ module.exports = class Message extends Event {
 				}
 			}
 
+			if (prefix == this.client.settings.prefix) {
+				if (cmd && !args[0] && cmd.args === true) return message.channel.send(`You didn't provide any arguments ${message.author}.\nCorrect Usage: \`ear ${cmd.name} ${cmd.usage}\``);
+			}
+			else if (cmd && !args[0] && cmd.args === true) {
+				return message.channel.send(`You didn't provide any arguments ${message.author}.\nCorrect Usage: \`${prefix} ${cmd.name} ${cmd.usage}\` or \`${prefix}${cmd.name} ${cmd.usage}\``);
+			}
+
 			try {
 				cmd.execute(this.client, message, args);
 			}
@@ -193,141 +200,5 @@ module.exports = class Message extends Event {
 				message.reply('There was an error trying to execute that command!');
 			}
 		});
-
-		// bad code ahead
-		/* eslint-disable */
-    return;
-    users.findOne({
-      authorID: message.author.id
-    }, async (err, u) => {
-      if (err) console.log(err);
-      if (!u) {
-        const newUser = new users({
-          authorID: message.author.id,
-          authorName: message.author.tag,
-          bio: "",
-          songsPlayed: 0,
-          commandsUsed: 1,
-          blocked: false,
-          premium: false,
-          pro: false,
-          developer: false,
-        });
-        await newUser.save().catch(e => console.log(e));
-      } else {
-        if (u.blocked == null) u.blocked = false;
-        if (u.blocked) ignoreMsg = true;
-        else if (!u.blocked) u.commandsUsed += 1;
-        await u.save().catch(e => console.log(e));
-      }
-
-      if (ignoreMsg) return;
-
-      const command = args.shift().toLowerCase();
-      const cmd = this.client.commands.get(command) || this.client.commands.find(c => c.aliases && c.aliases.includes(command));
-      if (!cmd) {
-        if (fs.existsSync(`./commands/${command}.js`)) {
-          try {
-            const commandFile = require(`./commands/${command}.js`);
-            if (commandFile) commandFile.run(this.client, message, args);
-          } catch (error) {
-            console.error(error);
-            message.reply("There was an error trying to execute that command!");
-          }
-        }
-        return;
-      }
-
-      bot.findOne({
-        clientID: this.client.user.id
-      }, async (err, b) => {
-        if (err) console.log(err);
-        if (!b) {
-          const newClient = new bot({
-            clientID: this.client.user.id,
-            clientName: this.client.user.name,
-            messagesSent: 0,
-            songsPlayed: 0,
-          });
-          await newClient.save().catch(e => console.log(e));
-        }
-
-        b.messagesSent += 1;
-        await b.save().catch(e => console.log(e));
-      });
-
-      commandsSchema.findOne({
-        commandName: cmd.name
-      }, async (err, c) => {
-        if (err) console.log(err);
-        if (!c) {
-          const newCommand = new commandsSchema({
-            commandName: cmd.name,
-            timesUsed: 1,
-          });
-          await newCommand.save().catch(e => console.log(e));
-        }
-
-        c.timesUsed += 1;
-        await c.save().catch(e => console.log(e));
-      });
-
-      console.log(`${cmd.name} used by ${message.author.tag} (${message.author.id}) from ${message.guild.name} (${message.guild.id})`);
-      const embed = new Discord.MessageEmbed()
-        .setAuthor(`${message.author.username}`, message.author.displayAvatarURL())
-        .setColor(this.client.colors.main)
-        .setDescription(`**${cmd.name}** command used by **${message.author.tag}** (${message.author.id})`)
-        .setFooter(`${message.guild.name} (${message.guild.id})`, message.guild.iconURL())
-        .setTimestamp();
-
-      webhookClient.send({
-        username: "Ear Tensifier",
-        avatarURL: this.client.settings.avatar,
-        embeds: [embed],
-      });
-
-      if (!cooldowns.has(cmd.name)) {
-        cooldowns.set(cmd.name, new Discord.Collection());
-      }
-      if (cmd.permission === "dev" && !this.client.settings.devs.includes(message.author.id)) return;
-
-      if (cmd && !message.guild && cmd.guildOnly) return message.channel.send("I can't execute that command inside DMs!. Please run this command in a server.");
-
-      if (!this.client.settings.devs.includes(message.author.id)) {
-        if (!cooldowns.has(cmd.name)) {
-          cooldowns.set(cmd.name, new Discord.Collection());
-        }
-        const now = Date.now();
-        const timestamps = cooldowns.get(cmd.name);
-        const cooldownAmount = (cmd.cooldown || 5) * 1000;
-        if (!timestamps.has(message.author.id)) {
-          timestamps.set(message.author.id, now);
-          setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
-        }
-        else {
-          const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
-          if (now < expirationTime) {
-            const timeLeft = (expirationTime - now) / 1000;
-            return message.reply(`Please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${cmd.name}\` command.`);
-          }
-          timestamps.set(message.author.id, now);
-          setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
-        }
-      }
-
-      if (prefix == this.client.settings.prefix) {
-        if (cmd && !args[0] && cmd.args === true) return message.channel.send(`You didn't provide any arguments ${message.author}.\nCorrect Usage: \`ear ${cmd.name} ${cmd.usage}\``);
-      }
-      else if (cmd && !args[0] && cmd.args === true) {
-        return message.channel.send(`You didn't provide any arguments ${message.author}.\nCorrect Usage: \`${prefix} ${cmd.name} ${cmd.usage}\` or \`${prefix}${cmd.name} ${cmd.usage}\``);
-      }
-
-      try {
-        cmd.execute(this.client, message, args);
-      } catch (e) {
-        console.error(e);
-        message.reply("There was an error trying to execute that command!");
-      }
-    });
-  }
+	}
 };
