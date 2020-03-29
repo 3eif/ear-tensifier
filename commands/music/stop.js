07bdcc7ea6f05cb3ea9bb4d5115d069f1aa920b4
Tetra <seifaziz10@gmail.com>
2020-03-29 08:12:01 -0700

    Update stop.js

--- commands/music/stop.js
+++ commands/music/stop.js
@@ -3,11 +3,10 @@ module.exports = {
 	description: 'Stops the queue.',
 	cooldown: '10',
 	async execute(client, message) {
-		const voiceChannel = message.member.voice;
 		const player = client.music.players.get(message.guild.id);
 
-		if(!voiceChannel) return client.responses('noVoiceChannel', message);
-		if(voiceChannel.channelID != message.guild.members.cache.get(client.user.id).voice.id) return client.responses('sameVoiceChannel', message);
+		if(!message.member.voice.channel) return client.responses('noVoiceChannel', message);
+		if(message.member.voice.channel.id != message.guild.members.cache.get(client.user.id).voice.id) return client.responses('sameVoiceChannel', message);
 
 		if(player) {
 			player.queue = [];
