0b821213350fd597d0db86f9c6ab4eb277ed07b2
Tetra <seifaziz10@gmail.com>
2020-03-29 07:50:23 -0700

    Bug fixes

--- commands/music/stop.js
+++ commands/music/stop.js
@@ -7,7 +7,7 @@ module.exports = {
 		const player = client.music.players.get(message.guild.id);
 
 		if(!voiceChannel) return client.responses('noVoiceChannel', message);
-		if(voiceChannel.id != message.guild.members.cache.get(client.user.id).voice.channel.id) return client.responses('sameVoiceChannel', message);
+		if(voiceChannel.channelID != message.guild.members.cache.get(client.user.id).voice.id) return client.responses('sameVoiceChannel', message);
 
 		if(player) {
 			player.queue = [];
