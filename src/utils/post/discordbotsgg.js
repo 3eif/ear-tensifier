6232dab35e36a202aadc7bdb348595f13f271cc4
Tetra <seifaziz10@gmail.com>
2020-03-30 15:35:18 -0700

    Update discordbotsgg.js

--- src/utils/post/discordbotsgg.js
+++ src/utils/post/discordbotsgg.js
@@ -5,7 +5,7 @@ const dbots = new discordbots(post['discordBotsGG']['id'], post['discordBotsGG']
 
 module.exports = async (client, servers, shards, shardID) => {
 	// eslint-disable-next-line no-unused-vars
-	dbots.postStats(servers, shards, shardID).then((client) => {
+	dbots.postStats(servers, shards, shardID).then(() => {
 		client.log('Posted bot stats to discordbots.gg');
 	}).catch((error) => client.log('An error occured while posting bot stats to discordbots.gg: ' + error));
 };
