468be871396f132f844d642b414f8f7f112647bc
Tetra <seifaziz10@gmail.com>
2020-03-30 18:16:21 -0700

    Fix typo

--- src/commands/filters/treblebass.js
+++ src/commands/filters/treblebass.js
@@ -2,8 +2,8 @@ const premium = require('../../utils/premium/premium.js');
 const Discord = require('discord.js');
 
 module.exports = {
-	name: 'trablebass',
-	description: 'Turns on trablebass filter',
+	name: 'treblebass',
+	description: 'Turns on treblebass filter',
 	aliases: ['tb'],
 	cooldown: '10',
 	inVoiceChannel: true,
@@ -17,21 +17,21 @@ module.exports = {
 
 		if (args[0] && (args[0].toLowerCase() == 'reset' || args[0].toLowerCase() == 'off')) {
 			player.setEQ(Array(13).fill(0).map((n, i) => ({ band: i, gain: 0.15 })));
-			const msg = await message.channel.send(`${client.emojiList.loading} Turning off **trablebass**. This may take a few seconds...`);
+			const msg = await message.channel.send(`${client.emojiList.loading} Turning off **treblebass**. This may take a few seconds...`);
 			const embed = new Discord.MessageEmbed()
 				.setAuthor(message.guild.name, message.guild.iconURL())
-				.setDescription('Trablebass off')
+				.setDescription('Treblebass off')
 				.setColor(client.colors.main);
 			await delay(5000);
 			return msg.edit('', embed);
 		}
 
-		player.setEQ(client.filters.trablebass);
+		player.setEQ(client.filters.treblebass);
 
-		const msg = await message.channel.send(`${client.emojiList.loading} Turning on **trablebass**. This may take a few seconds...`);
+		const msg = await message.channel.send(`${client.emojiList.loading} Turning on **treblebass**. This may take a few seconds...`);
 		const embed = new Discord.MessageEmbed()
 			.setAuthor(message.guild.name, message.guild.iconURL())
-			.setDescription('Trablebass on')
+			.setDescription('Treblebass on')
 			.setFooter('Reset filter: ear reset')
 			.setColor(client.colors.main);
 		await delay(5000);
