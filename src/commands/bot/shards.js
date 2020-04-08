d5ac8807bb76534d64bb53bb9c875a8080f0ef5a
Tetra <seifaziz10@gmail.com>
2020-04-08 06:38:25 -0700

    Shards & stats command update

--- src/commands/bot/shards.js
+++ src/commands/bot/shards.js
@@ -19,7 +19,8 @@ module.exports = {
         this.channels.cache.size,
         this.users.cache.size,
         (process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2),
-        this.music.players.size
+		this.music.players.size,
+		this.ws.ping
       ]`);
 
 		const embed = new Discord.MessageEmbed()
@@ -29,16 +30,22 @@ module.exports = {
 		let totalMusicStreams = 0;
 		shardInfo.forEach(i => {
 			const status = i[1] === 'process' ? client.emojiList.online : client.emojiList.offline;
-			embed.addField(`${status} Shard ${i[0]}`, `\`\`\`js
-Servers: ${i[2]}\nChannels: ${i[3]}\nUsers: ${i[4]}\nMemory: ${i[5]} mb\nMusic Streams: ${i[6]}\`\`\``, true);
+			embed.addField(`${status} Shard ${i[0] + 1}`, `\`\`\`js
+Servers: ${i[2]}\nChannels: ${i[3]}\nUsers: ${i[4]}\nMemory: ${i[5]} mb\nAPI Latency: ${i[7]} ms\nMusic Streams: ${i[6]}\`\`\``, true);
 			totalMusicStreams += i[6];
 		});
 
 		Promise.all(promises)
 			.then(results => {
+				let totalMemory = 0;
+				shardInfo.forEach(s => totalMemory += parseInt(s[5]));
+				let avgLatency = 0;
+				shardInfo.forEach(s => avgLatency += s[7]);
+				avgLatency = avgLatency / shardInfo.length;
 				const totalGuilds = results[0].reduce((prev, guildCount) => prev + guildCount, 0);
 				const totalMembers = results[1].reduce((prev, memberCount) => prev + memberCount, 0);
-				embed.addField('Total Stats', `**${totalGuilds}** servers - **${totalMembers}** members - **${totalMusicStreams}** music streams`);
+
+				embed.setFooter(`Total Servers: ${totalGuilds}\nTotal Users: ${totalMembers}\nAvg. Latency: ${avgLatency} ms\nTotal Streams: ${totalMusicStreams}\nTotal Memory: ${totalMemory.toFixed(2)} mb`);
 				message.channel.send(embed);
 			});
 	},
--- src/commands/bot/stats.js
+++ src/commands/bot/stats.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 const bot = require('../../models/bot.js');
+const cpuStat = require('cpu-stat');
 
 module.exports = {
 	name: 'stats',
@@ -8,7 +9,6 @@ module.exports = {
 	async execute(client, message) {
 
 		const msg = await message.channel.send(`${client.emojiList.loading} Gathering stats...`);
-		const used = process.memoryUsage().heapUsed / 1024 / 1024;
 		const totalSeconds = process.uptime();
 		const realTotalSecs = Math.floor(totalSeconds % 60);
 		const days = Math.floor((totalSeconds % 31536000) / 86400);
@@ -20,6 +20,22 @@ module.exports = {
 			client.shard.broadcastEval('this.guilds.cache.reduce((prev, guild) => prev + guild.memberCount, 0)'),
 		];
 
+		const shardInfo = await client.shard.broadcastEval(`[
+			this.shard.ids,
+			this.shard.mode,
+			this.guilds.cache.size,
+			this.channels.cache.size,
+			this.users.cache.size,
+			(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2),
+			this.music.players.size,
+			this.ws.ping
+		  ]`);
+
+		let totalMusicStreams = 0;
+		shardInfo.forEach(i => {
+			totalMusicStreams += i[6];
+		});
+
 		bot.findOne({
 			clientID: client.user.id,
 		}, async (err, b) => {
@@ -29,22 +45,32 @@ module.exports = {
 					const totalGuilds = results[0].reduce((prev, guildCount) => prev + guildCount, 0);
 					const totalMembers = results[1].reduce((prev, memberCount) => prev + memberCount, 0);
 
-					const statsEmbed = new Discord.MessageEmbed()
-						.setAuthor('Ear Tensifier', client.user.displayAvatarURL())
-						.setColor(client.colors.main)
-						.setThumbnail(client.settings.avatar)
-						.addField('Born On', client.user.createdAt)
-						.addField('Current Version', client.settings.version, true)
-						.addField('Servers', `${totalGuilds} servers`, true)
-						.addField('Members', `${totalMembers} members`, true)
-						.addField('Shards', `${parseInt(client.shard.ids) + 1}/${client.shard.count}`, true)
-						.addField('Memory Used', `${Math.round(used * 100) / 100}MB`, true)
-						.addField('Messages Sent', `${b.messagesSent} messages`, true)
-						.addField('Songs Played', `${b.songsPlayed} songs`, true)
-						.addField('Uptime', `${days} days, ${hours} hours, ${mins} minutes, and ${realTotalSecs} seconds`)
-						.setFooter(`Latency ${msg.createdTimestamp - message.createdTimestamp}ms`)
-						.setTimestamp();
-					return msg.edit('', statsEmbed);
+					let totalMemory = 0;
+					shardInfo.forEach(s => totalMemory += parseInt(s[5]));
+					let avgLatency = 0;
+					shardInfo.forEach(s => avgLatency += s[7]);
+					avgLatency = avgLatency / shardInfo.length;
+
+					cpuStat.usagePercent(function(err, percent) {
+						const statsEmbed = new Discord.MessageEmbed()
+							.setAuthor('Ear Tensifier', client.user.displayAvatarURL())
+							.setColor(client.colors.main)
+							.setThumbnail(client.settings.avatar)
+							.addField('Born On', client.user.createdAt)
+							.addField('Current Version', client.settings.version, true)
+							.addField('Servers', `${totalGuilds} servers`, true)
+							.addField('Members', `${totalMembers} members`, true)
+							.addField('Shards', `${parseInt(client.shard.ids) + 1}/${client.shard.count}`, true)
+							.addField('Memory Used', `${totalMemory.toFixed(2)} mb`, true)
+							.addField('CPU usage', `${percent.toFixed(2)}%`, true)
+							.addField('Messages Sent', `${b.messagesSent} messages`, true)
+							.addField('Songs Played', `${b.songsPlayed} songs`, true)
+							.addField('Music Streams', `${totalMusicStreams} streams`, true)
+							.addField('Uptime', `\`\`\`${days} days, ${hours} hours, ${mins} minutes, and ${realTotalSecs} seconds\`\`\``)
+							.setFooter(`Latency ${msg.createdTimestamp - message.createdTimestamp}ms`)
+							.setTimestamp();
+						return msg.edit('', statsEmbed);
+					});
 				})
 				.catch(console.error);
 		});
