4d1ee25c134bb81c9c57e5e9402e4cf7cb57e88f
Tetra <seifaziz10@gmail.com>
2020-04-07 19:29:57 -0700

    Join command bug fix

--- src/commands/music/join.js
+++ src/commands/music/join.js
@@ -4,16 +4,22 @@ module.exports = {
 	aliases: ['summon'],
 	async execute(client, message) {
 		const voiceChannel = message.member.voice;
-		if(!voiceChannel) return client.responses('noVoiceChannel', message);
+		if (!voiceChannel) return client.responses('noVoiceChannel', message);
 
 		const permissions = voiceChannel.channel.permissionsFor(client.user);
-		if(!permissions.has('CONNECT')) return client.responses('noPermissionConnect', message);
+		if (!permissions.has('CONNECT')) return client.responses('noPermissionConnect', message);
 
-		client.music.players.spawn({
-			guild: message.guild,
-			textChannel: message.channel,
-			voiceChannel: message.member.voice.channel,
-		});
+
+		if (!client.music.players.get(message.guild.id)) {
+			client.music.players.spawn({
+				guild: message.guild,
+				textChannel: message.channel,
+				voiceChannel: message.member.voice.channel,
+			});
+		}
+		else {
+			message.member.voice.channel.join();
+		}
 
 		return message.channel.send(`Joined ${client.emojiList.voice}**${message.member.voice.channel.name}**`);
 	},
