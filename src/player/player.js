20480aa944afb2914ae9c659b2244999616579d5
Tetra <seifaziz10@gmail.com>
2020-04-11 07:24:17 -0700

    Bug fixes

--- src/player/player.js
+++ src/player/player.js
@@ -5,15 +5,16 @@ const trackStart = require('./trackStart.js');
 const autoPlay = require('./queueEnd.js');
 
 module.exports = async (client) => {
-	client.music = new ErelaClient(client, tokens.nodes)
-		.on('nodeError', client.log)
-		.on('nodeConnect', () => client.log)
-		.on('queueEnd', player => {
-			// player.textChannel.send("");
-			// autoPlay(client, player);
-			return client.music.players.destroy(player.guild.id);
-		})
-		.on('trackStart', ({ textChannel }, { title, duration, author, uri, requester }) => {
-			trackStart(client, textChannel, title, duration, author, uri, requester);
-		});
+	client.music = new ErelaClient(client, tokens.nodes);
+	// eslint-disable-next-line no-unused-vars
+	client.music.on('nodeConnect', node => client.log('New node connected'));
+	client.music.on('nodeError', (error) => client.log(error));
+	client.music.on('queueEnd', player => {
+		// player.textChannel.send("");
+		// autoPlay(client, player);
+		return client.music.players.destroy(player.guild.id);
+	});
+	client.music.on('trackStart', ({ textChannel }, { title, duration, author, uri, requester }) => {
+		trackStart(client, textChannel, title, duration, author, uri, requester);
+	});
 };
