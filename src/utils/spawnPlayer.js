4e33cbabad322590e90f55bc94e6653279e6c734
Tetra <seifaziz10@gmail.com>
2020-04-12 19:27:27 -0700

    Update spawnPlayer.js

--- src/utils/spawnPlayer.js
+++ src/utils/spawnPlayer.js
@@ -5,7 +5,8 @@ module.exports = async (client, message) => {
         voiceChannel: message.member.voice.channel,
     });
 
-    message.guild.members.cache.get(client.user.id).voice.setSelfDeaf(true);
+    const permissions = message.member.voice.channel.permissionsFor(client.user);
+    if (permissions.has('DEAFEN_MEMBERS') || permissions.has('ADMINISTRATOR')) message.guild.members.cache.get(client.user.id).voice.setDeaf(true);
 
     return player;
 };
