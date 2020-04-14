ae60fcfd112048a56888670ffd4db338741d00fb
Tetra <seifaziz10@gmail.com>
2020-04-13 18:26:55 -0700

    Update voted.js

--- src/utils/votes/voted.js
+++ src/utils/votes/voted.js
@@ -1,7 +1,5 @@
 module.exports = async (client, user) => {
 	client.dbl.hasVoted(user.id).then(voted => {
-		console.log(voted);
-		if (voted) { return true; }
-		else { return false; }
+		return voted;
 	});
 };
