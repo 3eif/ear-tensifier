64398488b9debf8194ca8625acdf9a64e1bf29fe
Tetra <seifaziz10@gmail.com>
2020-04-13 19:23:08 -0700

    Update getVotes.js

--- src/utils/votes/getVotes.js
+++ src/utils/votes/getVotes.js
@@ -1,5 +1,42 @@
-module.exports = async (client) => {
-	client.dbl.getVotes().then(votes => {
-		return votes;
+// module.exports = async (client) => {
+// 	client.dbl.getVotes().then(votes => {
+// 		return votes;
+// 	});
+// };
+
+const users = require('../../models/user.js');
+
+module.exports = async (client, author) => {
+	users.findOne({
+		authorID: author.id,
+	}, async (err, u) => {
+		if (err) console.log(err);
+		if (!u) {
+			const newUser = new users({
+				authorID: author.id,
+				bio: '',
+				songsPlayed: 0,
+				commandsUsed: 0,
+				blocked: false,
+				premium: false,
+				pro: false,
+				developer: false,
+				voted: false,
+			});
+			await newUser.save().catch(e => console.log(e));
+			return false;
+		}
+		else {
+			if (!u.voted) { return false; }
+			else if (u.voted) {
+				if (client.settings.voteCooldown - (Date.now() - u.lastVoted) > 0) {
+					return true;
+				}
+				else {
+					u.voted = false;
+				}
+			}
+			await u.save().catch(e => console.log(e));
+		}
 	});
 };
