13f0ba5ec3437197fbd40127464c1464196ec327
Tetra <seifaziz10@gmail.com>
2020-04-14 09:38:19 -0700

    Update getVoted.js

--- src/utils/votes/getVoted.js
+++ src/utils/votes/getVoted.js
@@ -5,45 +5,17 @@
 const users = require('../../models/user.js');
 
 module.exports = async (client, author) => {
-	async function getVoted() {
-		users.findOne({
-			authorID: author.id,
-		}, async (err, u) => {
-			if (err) console.log(err);
-			if (!u) {
-				const newUser = new users({
-					authorID: author.id,
-					bio: '',
-					songsPlayed: 0,
-					commandsUsed: 0,
-					blocked: false,
-					premium: false,
-					pro: false,
-					developer: false,
-					voted: false,
-				});
-				await newUser.save().catch(e => console.log(e));
-				return await false;
-			}
-			else {
-				if (!u.voted) {
-					return await u.voted;
-				}
-				else if (u.voted) {
-					if (client.settings.voteCooldown - (Date.now() - u.lastVoted) > 0) {
-						return await u.voted;
-					}
-					else {
-						u.voted = false;
-						return await u.voted;
-					}
-				}
-				await u.save().catch(e => console.log(e));
-			}
-		});
+	const u = await users.findOne({ authorID: author.id });
+	if(!u) return false;
+	if(!u.voted) return false;
+	if(u.voted) {
+		if (client.settings.voteCooldown - (Date.now() - u.lastVoted) > 0) {
+			return await u.voted;
+		}
+		else {
+			u.voted = false;
+			await u.save().catch(e => console.log(e));
+			return await u.voted;
+		}
 	}
-
-	(async () => {
-		console.log(await getVoted());
-	})();
 };
