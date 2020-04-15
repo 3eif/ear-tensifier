e414b925d06d64c07a2ba7f7c3f45a6d154a0fcd
Tetra <seifaziz10@gmail.com>
2020-04-15 09:22:00 -0700

    Bug fixes

--- src/commands/favorites/load.js
+++ src/commands/favorites/load.js
@@ -20,7 +20,7 @@ module.exports = class Load extends Command {
 		if (!permissions.has('SPEAK')) return message.channel.send('I do not have permission to speak in your voice channel.');
 
 		let player = client.music.players.get(message.guild.id);
-		if (player.playing == false && player) return message.channel.send(`Cannot play/queue songs while paused. Do \`${client.settings.prefix} resume\` to play.`);
+		if (player && player.playing == false) return message.channel.send(`Cannot play/queue songs while paused. Do \`${client.settings.prefix} resume\` to play.`);
 		if (!player) player = await spawnPlayer(client, message);
 
 		if (await songLimit() == patreon.defaultMaxSongs && player.queue.size >= patreon.defaultMaxSongs) return msg.edit(`You have reached the **maximum** amount of songs (${patreon.defaultMaxSongs} songs). Want more songs? Consider donating here: https://www.patreon.com/eartensifier`);
