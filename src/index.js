3cc21110c36297b8d8fda3c6e56e6f6d607bbaa0
Tetra <seifaziz10@gmail.com>
2020-04-15 17:27:04 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ mongoose.connect(`mongodb://${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/
 });
 
 let token;
+console.log(process.env.NODE_ENV);
 if(process.env.NODE_ENV == 'production') token = process.env.DISCORD_TOKEN;
 else token = process.env.DISCORD_TESTING_TOKEN;
 
