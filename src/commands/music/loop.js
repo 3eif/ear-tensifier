8ba872be1baa703f6f6f397be26afe512f8187f4
Tetra <seifaziz10@gmail.com>
2020-05-15 14:06:35 -0700

    Fixed loop queue

--- src/commands/music/loop.js
+++ src/commands/music/loop.js
@@ -16,24 +16,24 @@ module.exports = class Loop extends Command {
 	async run(client, message, args) {
 		const player = client.music.players.get(message.guild.id);
 
-		if(!args[0] || args[0].toLowerCase() == 'song') {
-			if(player.trackRepeat === false) {
+		if (!args[0] || args[0].toLowerCase() == 'song') {
+			if (player.trackRepeat == false) {
 				player.setTrackRepeat(true);
 				return message.channel.send('Song is now being looped');
 			}
-			else{
+			else {
 				player.setTrackRepeat(false);
 				return message.channel.send('Song has been unlooped');
 			}
 		}
-		else if(args[0] == 'queue') {
-			if(player.setQueueRepeat) {
+		else if (args[0] == 'queue') {
+			if (player.queueRepeat == true) {
 				player.setQueueRepeat(false);
 				return message.channel.send('Queue has been unlooped.');
 			}
 			else {
 				player.setQueueRepeat(true);
-				return message.channel.send('Queue is now being looped.');
+				return message.channel.send('Queue is being looped.');
 			}
 		}
 	}
