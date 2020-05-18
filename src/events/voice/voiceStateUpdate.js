1dc0ac5cc93c811083d48ed8a146db258bca989b
Tetra <seifaziz10@gmail.com>
2020-05-17 18:47:56 -0700

    Clean voice event

--- src/events/voice/voiceStateUpdate.js
+++ src/events/voice/voiceStateUpdate.js
@@ -12,7 +12,7 @@ module.exports = class VoiceStateUpdate extends Event {
 
 		if (!player) return;
 
-		if (player && !newVoice.guild.members.cache.get(this.client.user.id).voice.channelID) this.client.music.players.destroy(oldVoice.guild.id);
+		if (!newVoice.guild.members.cache.get(this.client.user.id).voice.channelID) this.client.music.players.destroy(oldVoice.guild.id);
 		if (oldVoice.id === this.client.user.id) return;
 		if (!oldVoice.guild.members.cache.get(this.client.user.id).voice.channelID) return;
 		if (oldVoice.guild.members.cache.get(this.client.user.id).voice.channel.id === oldVoice.channelID) {
