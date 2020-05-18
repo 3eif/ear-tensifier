9e73426aabefa436bcae4bb9d9d5ad9320808669
Tetra <seifaziz10@gmail.com>
2020-05-17 18:51:32 -0700

    Clean getVoted util

--- src/utils/getVoted.js
+++ src/utils/getVoted.js
@@ -4,14 +4,12 @@ module.exports = async (client, author) => {
 	const u = await users.findOne({ authorID: author.id });
 	if (!u) return false;
 	if (!u.voted) return false;
-	if (u.voted) {
-		if (client.settings.voteCooldown - (Date.now() - u.lastVoted) > 0) {
-			u.voted = false;
-			await u.save().catch(e => console.log(e));
-			return await u.voted;
-		}
-		else {
-			return await u.voted;
-		}
+	if (client.settings.voteCooldown - (Date.now() - u.lastVoted) > 0) {
+		u.voted = false;
+		await u.save().catch(e => console.log(e));
+		return await u.voted;
+	}
+	else {
+		return await u.voted;
 	}
 };
