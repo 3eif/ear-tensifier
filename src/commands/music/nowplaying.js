11d936c1692cdc66aa20c0c7392425b2b0369c2e
Tetra <seifaziz10@gmail.com>
2020-05-29 16:48:04 -0700

    Bug fixes

--- src/commands/music/nowplaying.js
+++ src/commands/music/nowplaying.js
@@ -16,20 +16,23 @@ module.exports = class NowPlaying extends Command {
 	}
 	async run(client, message) {
 		const player = client.music.players.get(message.guild.id);
-		const { title, author, duration, requester, uri } = player.queue[0];
+		const { title, author, duration, requester, uri, identifier } = player.queue[0];
 
 		const parsedCurrentDuration = moment.duration(player.position, 'milliseconds').format('hh:mm:ss', { trim: false });
 		const parsedDuration = moment.duration(duration, 'milliseconds').format('hh:mm:ss', { trim: false });
 		const part = Math.floor((player.position / duration) * 30);
 		const uni = player.playing ? '▶' : '⏸️';
 
+		const thumbnail = `https://img.youtube.com/vi/${identifier}/mqdefault.jpg`;
+		const user = `<@${requester.id}>`;
+
 		const embed = new Discord.MessageEmbed()
 			.setColor(client.colors.main)
 			.setAuthor(player.playing ? 'Now Playing' : 'Paused', 'https://cdn.discordapp.com/emojis/673357192203599904.gif?v=1')
-			.setThumbnail(player.queue[0].displayThumbnail('default'))
+			.setThumbnail(thumbnail)
 			.setDescription(`**[${title}](${uri})**`)
 			.addField('Author', author, true)
-			.addField('Requested by', requester, true)
+			.addField('Requested by', user, true)
 			.addField(`Duration \`[${parsedDuration}]\``, `\`\`\`${uni} ${'─'.repeat(part) + '⚪' + '─'.repeat(30 - part)} ${parsedCurrentDuration}\`\`\``);
 
 		return message.channel.send('', embed);
--- src/player/trackStart.js
+++ src/player/trackStart.js
@@ -10,7 +10,7 @@ momentDurationFormatSetup(moment);
 module.exports = async (client, textChannel, title, duration, author, uri) => {
 	const currentSong = client.music.players.get(textChannel.guild.id).queue[0];
 	const requester = `<@${currentSong.requester.id}>`;
-	const thumbnail = `https://img.youtube.com/vi/${currentSong.identifier}/default.jpg`;
+	const thumbnail = `https://img.youtube.com/vi/${currentSong.identifier}/mqdefault.jpg`;
 	addDB(uri, title, author, duration, uri, thumbnail);
 
 	// bot.findOne({
