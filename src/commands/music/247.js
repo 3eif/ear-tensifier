f9b7d52c20de145c69612353971d02118ab5e7fd
Tetra <seifaziz10@gmail.com>
2020-05-30 11:22:19 -0700

    Create 247.js

---
+++ src/commands/music/247.js
@@ -0,0 +1,18 @@
+const Command = require('../../structures/Command');
+
+module.exports = class TwentyFourSeven extends Command {
+	constructor(client) {
+		super(client, {
+			name: '24/7',
+			description: 'Stays in the voice channel even if no one is in it.',
+			aliases: ['stay', '247'],
+			playing: true,
+		});
+	}
+	async run(client, message) {
+		const player = client.music.players.get(message.guild.id);
+		player.twentyFourSeven = true;
+
+		return message.channel.send('24/7 mode is now on.');
+	}
+};
