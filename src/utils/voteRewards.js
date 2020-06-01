10f3dc33b2c049ae25dbe4d651183d5970d4c258
Tetra <seifaziz10@gmail.com>
2020-06-01 11:36:22 -0700

    Update voteRewards.js

--- src/utils/voteRewards.js
+++ src/utils/voteRewards.js
@@ -29,7 +29,7 @@ module.exports = async (client, user) => {
         else {
             if(!Number.isInteger(u.votedTimes)) u.votedTimes = 1;
             else u.votedTimes++;
-            lastVotedTime = Date(u.lastVoted).toString();
+            lastVotedTime = moment(u.lastVotedTime, 'MM/DD/YYYY', true).format();
             console.log(lastVotedTime);
             u.lastVoted = Date.now();
             u.voted = true;
