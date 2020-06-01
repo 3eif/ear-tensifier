cfeeeecefcfacadbb7f29ff7761a30cec737e060
Tetra <seifaziz10@gmail.com>
2020-06-01 12:04:19 -0700

    Update getVoted.js

--- src/utils/getVoted.js
+++ src/utils/getVoted.js
@@ -4,7 +4,7 @@ module.exports = async (client, author) => {
 	const u = await users.findOne({ authorID: author.id });
 	if (!u) return false;
 	if (!u.voted) return false;
-	if (client.settings.voteCooldown - (Date.now() - u.lastVoted) > 0) {
+	if ((Date.now() + client.settings.voteCooldown) - (Date.now() - u.lastVoted) > 0) {
 		u.voted = false;
 		await u.save().catch(e => console.log(e));
 		return await u.voted;
