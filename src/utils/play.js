69f08deaf53e465939680f84349f077b1f1b386d
Tetra <seifaziz10@gmail.com>
2020-06-03 19:37:35 -0700

    Update play.js

--- src/utils/play.js
+++ src/utils/play.js
@@ -6,47 +6,65 @@ momentDurationFormatSetup(moment);
 
 module.exports = async (client, message, msg, player, searchQuery, playlist) => {
 	// eslint-disable-next-line no-unused-vars
-	let failedCount = 0;
-
-	function getRes() {
-		client.music.search(searchQuery, message.author).then(async res => {
-			return res.loadType;
-		}).catch(err => {
-			if (playlist) return;
+	function play() {
+		return new Promise(async function(resolve, reject) {
+			client.music.search(searchQuery, message.author).then(async res => {
+				if (res.loadType == 'TRACK_LOADED') {
+					player.queue.add(res.tracks[0]);
+					if (!playlist && msg) msg.edit(`**${res.tracks[0].title}** (${moment.duration(res.tracks[0].duration, 'milliseconds').format('hh:mm:ss', { trim: false })}) has been added to the queue by **${res.tracks[0].requester.tag}**`);
+					if (!player.playing) player.play();
+					resolve();
+				}
+				else if (res.loadType == 'SEARCH_RESULT') {
+					player.queue.add(res.tracks[0]);
+					if (!playlist && msg) msg.edit(`**${res.tracks[0].title}** (${moment.duration(res.tracks[0].duration, 'milliseconds').format('hh:mm:ss', { trim: false })}) has been added to the queue by **${res.tracks[0].requester.tag}**`);
+					if (!player.playing) player.play();
+					resolve();
+				}
+				else if (res.loadType == 'PLAYLIST_LOADED') {
+					for (const track of res.playlist.tracks) {
+						player.queue.add(track);
+						if (!player.playing && player.queue.length == 1) player.play();
+					}
+					msg.edit(`**${res.playlist.info.name}** (${moment.duration(res.playlist.tracks.reduce((acc, cure) => ({ duration: acc.duration + cure.duration })).duration, 'milliseconds').format('hh:mm:ss', { trim: false })}) (${res.playlist.tracks.length} tracks) has been added to the queue by **${res.playlist.tracks[0].requester.tag}**`);
+					resolve();
+				}
+				else if (res.loadType == 'NO_MATCHES') {
+					if (res.loadType == 'TRACK_LOADED') {
+						player.queue.add(res.tracks[0]);
+						if (!playlist && msg) msg.edit(`**${res.tracks[0].title}** (${moment.duration(res.tracks[0].duration, 'milliseconds').format('hh:mm:ss', { trim: false })}) has been added to the queue by **${res.tracks[0].requester.tag}**`);
+						if (!player.playing) player.play();
+						resolve();
+					}
+					else if (res.loadType == 'SEARCH_RESULT') {
+						player.queue.add(res.tracks[0]);
+						if (!playlist && msg) msg.edit(`**${res.tracks[0].title}** (${moment.duration(res.tracks[0].duration, 'milliseconds').format('hh:mm:ss', { trim: false })}) has been added to the queue by **${res.tracks[0].requester.tag}**`);
+						if (!player.playing) player.play();
+						resolve();
+					}
+					else if (res.loadType == 'PLAYLIST_LOADED') {
+						for (const track of res.playlist.tracks) {
+							player.queue.add(track);
+							if (!player.playing && player.queue.length == 1) player.play();
+						}
+						msg.edit(`**${res.playlist.info.name}** (${moment.duration(res.playlist.tracks.reduce((acc, cure) => ({ duration: acc.duration + cure.duration })).duration, 'milliseconds').format('hh:mm:ss', { trim: false })}) (${res.playlist.tracks.length} tracks) has been added to the queue by **${res.playlist.tracks[0].requester.tag}**`);
+						resolve();
+					}
+					else if (res.loadType == 'NO_MATCHES') {
+						msg.edit('No tracks found. Please try again.');
+					}
+					else if (res.loadType == 'LOAD_FAILED') {
+						msg.edit('No tracks found. Please try again.');
+					}
+				}
+				else if (res.loadType == 'LOAD_FAILED') {
+					msg.edit('No tracks found. Please try again.');
+				}
+			}).catch(err => {
+				if (playlist) return;
+				reject();
+			});
 		});
 	}
-
-	console.log(getRes());
-
-	while(getRes().loadType == 'NO_MATCHES') {
-		if(failedCount > 5) {
-			msg.edit('No tracks found. Please try again.');
-			break;
-		}
-		else {
-			getRes();
-			failedCount++;
-		}
-	}
-
-	if (getRes().loadType == 'TRACK_LOADED') {
-		player.queue.add(getRes().tracks[0]);
-		if (!playlist && msg) msg.edit(`**${getRes().tracks[0].title}** (${moment.duration(getRes().tracks[0].duration, 'milliseconds').format('hh:mm:ss', { trim: false })}) has been added to the queue by **${getRes().tracks[0].requester.tag}**`);
-		if (!player.playing) player.play();
-	}
-	else if (getRes().loadType == 'SEARCH_getRes()ULT') {
-		player.queue.add(getRes().tracks[0]);
-		if (!playlist && msg) msg.edit(`**${getRes().tracks[0].title}** (${moment.duration(getRes().tracks[0].duration, 'milliseconds').format('hh:mm:ss', { trim: false })}) has been added to the queue by **${getRes().tracks[0].requester.tag}**`);
-		if (!player.playing) player.play();
-	}
-	else if (getRes().loadType == 'PLAYLIST_LOADED') {
-		for (const track of getRes().playlist.tracks) {
-			player.queue.add(track);
-			if (!player.playing && player.queue.length == 1) player.play();
-		}
-		msg.edit(`**${getRes().playlist.info.name}** (${moment.duration(getRes().playlist.tracks.reduce((acc, cure) => ({ duration: acc.duration + cure.duration })).duration, 'milliseconds').format('hh:mm:ss', { trim: false })}) (${getRes().playlist.tracks.length} tracks) has been added to the queue by **${getRes().playlist.tracks[0].requester.tag}**`);
-	}
-	else if(getRes().loadType == 'LOAD_FAILED') {
-		msg.edit('No tracks found. Please try again.');
-	}
+	play();
 };
