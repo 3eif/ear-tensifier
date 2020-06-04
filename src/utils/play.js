9d4aadba70e442fca11ce9dcf0f1a22db9e18224
Tetra <seifaziz10@gmail.com>
2020-06-03 19:39:46 -0700

    Update play.js

--- src/utils/play.js
+++ src/utils/play.js
@@ -6,6 +6,8 @@ momentDurationFormatSetup(moment);
 
 module.exports = async (client, message, msg, player, searchQuery, playlist) => {
 	// eslint-disable-next-line no-unused-vars
+	let failedCount = 0;
+
 	function play() {
 		return new Promise(async function(resolve, reject) {
 			client.music.search(searchQuery, message.author).then(async res => {
@@ -30,35 +32,18 @@ module.exports = async (client, message, msg, player, searchQuery, playlist) => 
 					resolve();
 				}
 				else if (res.loadType == 'NO_MATCHES') {
-					if (res.loadType == 'TRACK_LOADED') {
-						player.queue.add(res.tracks[0]);
-						if (!playlist && msg) msg.edit(`**${res.tracks[0].title}** (${moment.duration(res.tracks[0].duration, 'milliseconds').format('hh:mm:ss', { trim: false })}) has been added to the queue by **${res.tracks[0].requester.tag}**`);
-						if (!player.playing) player.play();
-						resolve();
-					}
-					else if (res.loadType == 'SEARCH_RESULT') {
-						player.queue.add(res.tracks[0]);
-						if (!playlist && msg) msg.edit(`**${res.tracks[0].title}** (${moment.duration(res.tracks[0].duration, 'milliseconds').format('hh:mm:ss', { trim: false })}) has been added to the queue by **${res.tracks[0].requester.tag}**`);
-						if (!player.playing) player.play();
-						resolve();
-					}
-					else if (res.loadType == 'PLAYLIST_LOADED') {
-						for (const track of res.playlist.tracks) {
-							player.queue.add(track);
-							if (!player.playing && player.queue.length == 1) player.play();
-						}
-						msg.edit(`**${res.playlist.info.name}** (${moment.duration(res.playlist.tracks.reduce((acc, cure) => ({ duration: acc.duration + cure.duration })).duration, 'milliseconds').format('hh:mm:ss', { trim: false })}) (${res.playlist.tracks.length} tracks) has been added to the queue by **${res.playlist.tracks[0].requester.tag}**`);
-						resolve();
-					}
-					else if (res.loadType == 'NO_MATCHES') {
+					if(failedCount > 5) {
 						msg.edit('No tracks found. Please try again.');
+						reject();
 					}
-					else if (res.loadType == 'LOAD_FAILED') {
-						msg.edit('No tracks found. Please try again.');
+					else {
+						play();
+						failedCount++;
 					}
 				}
-				else if (res.loadType == 'LOAD_FAILED') {
+				else if(res.loadType == 'LOAD_FAILED') {
 					msg.edit('No tracks found. Please try again.');
+					reject();
 				}
 			}).catch(err => {
 				if (playlist) return;
@@ -66,5 +51,6 @@ module.exports = async (client, message, msg, player, searchQuery, playlist) => 
 			});
 		});
 	}
+
 	play();
 };
