5f128461fe9a09261e39607236e4aaa15e9a259d
Tetra <seifaziz10@gmail.com>
2020-06-11 14:23:34 -0700

    Update getQueueDuration.js

--- src/utils/music/getQueueDuration.js
+++ src/utils/music/getQueueDuration.js
@@ -1,5 +1,5 @@
 module.exports = async (player) => {
-    if (!player.queue.length) return player.queue.current.length;
+    if (player.queue.length == 0) return player.current.length;
 
     let totalQueueDuration = 0;
     for(let i = 0; i < player.queue.length; i++) {
