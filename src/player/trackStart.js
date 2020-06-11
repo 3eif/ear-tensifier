fed24a71557a8dd2f3395b608a18be1319f63758
Tetra <seifaziz10@gmail.com>
2020-06-11 16:05:37 -0700

    F

--- src/player/trackStart.js
+++ src/player/trackStart.js
@@ -82,6 +82,7 @@ module.exports = async (client, textChannel, title, length, author, uri) => {
 		embed.setFooter('Other');
 	}
 
+	embed.setTitle(author);
 	const currentDuration = client.music.players.get(textChannel.guild.id).position;
 	const playing = client.music.players.get(textChannel.guild.id).playing;
 	const parsedCurrentDuration = moment.duration(currentDuration, 'milliseconds').format('mm:ss', { trim: false });
@@ -89,11 +90,11 @@ module.exports = async (client, textChannel, title, length, author, uri) => {
 	const part = Math.floor((currentDuration / length) * client.settings.embedDurationLength);
 	const uni = playing ? '▶' : '⏸️';
 
-	embed.addField('Author', author, true);
+	embed.addField('Duration', `\`${parsedCurrentDuration}/${parsedDuration}\``, true);
 	embed.setDescription(`**[${title}](${uri})**`);
 	embed.addField('Requested by', requester, true);
 
-	embed.addField(`Duration \`${parsedCurrentDuration}/${parsedDuration}\``, `\`\`\`${uni} ${'─'.repeat(part) + '⚪' + '─'.repeat(client.settings.embedDurationLength - part)}\`\`\``);
+	// embed.addField(`Duration \`${parsedCurrentDuration}/${parsedDuration}\``, `\`\`\`${uni} ${'─'.repeat(part) + '⚪' + '─'.repeat(client.settings.embedDurationLength - part)}\`\`\``);
 	embed.setTimestamp();
 
 	return textChannel.send(embed);
