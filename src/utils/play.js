77f9e4f72287111c8686cdc769c55e36a5baced7
Tetra <seifaziz10@gmail.com>
2020-06-13 18:31:08 -0700

    Update play.js

--- src/utils/play.js
+++ src/utils/play.js
@@ -31,41 +31,39 @@ module.exports = async (client, message, msg, player, searchQuery, playlist) => 
 					resolve();
 				}
 				else if (res.loadType == 'NO_MATCHES') {
-					// client.music.search(searchQuery, message.author).then(async res2 => {
-					// 	if (res2.loadType == 'TRACK_LOADED') {
-					// 		player.queue.add(res2.tracks[0]);
-					// 		if (!playlist && msg) msg.edit(`**${res2.tracks[0].title}** (${moment.duration(res2.tracks[0].length, 'milliseconds').format('mm:ss', { trim: false })}) has been added to the queue by **${res2.tracks[0].requester.tag}**`);
-					// 		if (!player.playing && !player.paused && !player.queue.length) player.play();
-					// 		resolve();
-					// 	}
-					// 	else if (res2.loadType == 'SEARCH_RESULT') {
-					// 		player.queue.add(res2.tracks[0]);
-					// 		if (!playlist && msg) msg.edit(`**${res2.tracks[0].title}** (${moment.duration(res2.tracks[0].length, 'milliseconds').format('mm:ss', { trim: false })}) has been added to the queue by **${res2.tracks[0].requester.tag}**`);
-					// 		if (!player.playing && !player.paused && !player.queue.length) player.play();
-					// 		resolve();
-					// 	}
-					// 	else if (res2.loadType == 'PLAYLIST_LOADED') {
-					// 		for (const track of res2.playlist.tracks) {
-					// 			player.queue.add(track);
-					// 			if (!player.playing && !player.paused && !player.queue.length) player.play();
-					// 		}
-					// 		msg.edit(`**${res2.playlist.info.name}** (${moment.duration(res2.playlist.tracks.reduce((acc, cure) => ({ duration: acc.length + cure.length })).duration, 'milliseconds').format('mm:ss', { trim: false })}) (${res2.playlist.tracks.length} tracks) has been added to the queue by **${res2.playlist.tracks[0].requester.tag}**`);
-					// 		resolve();
-					// 	}
-					// 	else if (res2.loadType == 'NO_MATCHES') {
-					// 		msg.edit('No tracks found. Please try again.');
-					// 		reject();
-					// 	}
-					// 	else if(res2.loadType == 'LOAD_FAILED') {
-					// 		msg.edit('No tracks found. Please try again.');
-					// 		reject();
-					// 	}
-					// }).catch(err => {
-					// 	if (playlist) return;
-					// 	reject();
-					// });
-					msg.edit('No tracks found. Please try again.');
-					reject();
+					client.music.search(searchQuery, message.author).then(async res2 => {
+						if (res2.loadType == 'TRACK_LOADED') {
+							player.queue.add(res2.tracks[0]);
+							if (!playlist && msg) msg.edit(`**${res2.tracks[0].title}** (${moment.duration(res2.tracks[0].length, 'milliseconds').format('mm:ss', { trim: false })}) has been added to the queue by **${res2.tracks[0].requester.tag}**`);
+							if (!player.playing && !player.paused && !player.queue.length) player.play();
+							resolve();
+						}
+						else if (res2.loadType == 'SEARCH_RESULT') {
+							player.queue.add(res2.tracks[0]);
+							if (!playlist && msg) msg.edit(`**${res2.tracks[0].title}** (${moment.duration(res2.tracks[0].length, 'milliseconds').format('mm:ss', { trim: false })}) has been added to the queue by **${res2.tracks[0].requester.tag}**`);
+							if (!player.playing && !player.paused && !player.queue.length) player.play();
+							resolve();
+						}
+						else if (res2.loadType == 'PLAYLIST_LOADED') {
+							for (const track of res2.playlist.tracks) {
+								player.queue.add(track);
+								if (!player.playing && !player.paused && !player.queue.length) player.play();
+							}
+							msg.edit(`**${res2.playlist.info.name}** (${moment.duration(res2.playlist.tracks.reduce((acc, cure) => ({ duration: acc.length + cure.length })).duration, 'milliseconds').format('mm:ss', { trim: false })}) (${res2.playlist.tracks.length} tracks) has been added to the queue by **${res2.playlist.tracks[0].requester.tag}**`);
+							resolve();
+						}
+						else if (res2.loadType == 'NO_MATCHES') {
+							msg.edit('No tracks found. Please try again.');
+							reject();
+						}
+						else if(res2.loadType == 'LOAD_FAILED') {
+							msg.edit('No tracks found. Please try again.');
+							reject();
+						}
+					}).catch(err => {
+						if (playlist) return;
+						reject();
+					});
 				}
 				else if(res.loadType == 'LOAD_FAILED') {
 					msg.edit('No tracks found. Please try again.');
