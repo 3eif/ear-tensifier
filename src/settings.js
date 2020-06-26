c7c468966630821f25976d63fd64b4a1b66ac93f
Tetra <seifaziz10@gmail.com>
2020-06-26 06:26:32 -0700

    Update version

--- src/settings.js
+++ src/settings.js
@@ -1,7 +1,7 @@
 module.exports = {
 	'prefix': 'ear ',
 	'testing': false,
-	'version': '1.5.1',
+	'version': '1.5.2',
 	'server': 'https://discord.gg/xKgKMAP',
 	'voiceLeave': '60000',
 	'voteCooldown': 43200000,
