081cc72672e0444fc032084e7ad795a143c2d7b1
Tetra <seifaziz10@gmail.com>
2020-07-05 08:08:25 -0700

    Update blapi.js

--- src/utils/post/blapi.js
+++ src/utils/post/blapi.js
@@ -2,14 +2,14 @@ const blapi = require('blapi');
 const botLists = require('../../../config/botlists.json');
 
 module.exports = async (client, servers, shards, shardCount) => {
-    // post();
-    // setInterval(function() {
-    //     post();
-    // }, 1800000);
+    post();
+    setInterval(function() {
+        post();
+    }, 1800000);
 
-    // function post() {
-    //     blapi.setLogging(true);
-    //     blapi.manualPost(servers, client.user.id, botLists, null, shardCount, shards);
-    //     client.log('Posted bot stats to bot lists.');
-    // }
+    function post() {
+        blapi.setLogging(true);
+        blapi.manualPost(servers, client.user.id, botLists, null, shardCount, shards);
+        client.log('Posted bot stats to bot lists.');
+    }
 };
