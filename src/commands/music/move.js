bcb2bdd4a55acd716c7fad7dd9cb0f298fa19827
Tetra <seifaziz10@gmail.com>
2020-07-07 13:33:31 -0700

    Move command

--- src/commands/music/move.js
+++ src/commands/music/move.js
@@ -18,21 +18,21 @@ module.exports = class Move extends Command {
 		if (args[0] === 0) return message.channel.send(`Cannot move a song that is already playing. To skip the current playing song type: \`${client.settings.prefix}skip\``);
 
 		const player = client.music.players.get(message.guild.id);
-		if ((args[0] > player.queue.length) || (args[0] && !player.queue[args[0]])) return message.channel.send('Song not found.');
+		if ((args[0] > player.queue.length) || (args[0] && !player.queue.next[args[0] - 1])) return message.channel.send('Song not found.');
 
 		if (!args[1]) {
-			const song = player.queue[args[0] - 1];
-			player.queue.splice(args[0] - 1, 1);
-			player.queue.splice(0, 0, song);
-			return message.channel.send(`Moved **${song.title}** to the beginning of the queue.`);
+			const song = player.queue.next[args[0] - 1];
+			player.queue.next.splice(args[0] - 1, 1);
+			player.queue.next.splice(0, 0, song);
+			return message.channel.send(`Moved **${client.decode(song.song).title}** to the beginning of the queue.`);
 		}
 		else if (args[1]) {
 			if (args[1] == 0) return message.channel.send(`Cannot move a song that is already playing. To skip the current playing song type: \`${client.settings.prefix}skip\``);
-			if ((args[1] > player.queue.length) || !player.queue[args[1]]) return message.channel.send('Song not found.');
-			const song = player.queue[args[0] - 1];
-			player.queue.splice(args[0] - 1, 1);
-			player.queue.splice(args[1] - 1, 0, song);
-			return message.channel.send(`Moved **${song.title}** to the position ${args[1]}.`);
+			if ((args[1] > player.queue.length) || !player.queue.next[args[1] - 1]) return message.channel.send('Song not found.');
+			const song = player.queue.next[args[0] - 1];
+			player.queue.next.splice(args[0] - 1, 1);
+			player.queue.next.splice(args[1] - 1, 0, song);
+			return message.channel.send(`Moved **${client.decode(song.song).title}** to position: **${args[1]}**.`);
 		}
 	}
 };
