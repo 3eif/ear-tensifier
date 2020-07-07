7034bd4947549871e2b36cf7274b923595c66efb
Tetra <seifaziz10@gmail.com>
2020-07-07 13:23:14 -0700

    Queue command PHEW!

--- src/commands/music/queue.js
+++ src/commands/music/queue.js
@@ -25,49 +25,36 @@ module.exports = class Queue extends Command {
 		const parsedDuration = client.formatDuration(length);
 		const parsedQueueDuration = client.formatDuration(getQueueDuration(player));
 		let pagesNum = Math.ceil(player.queue.length / 10);
-		if(pagesNum === 0) pagesNum = 1;
+		if (pagesNum === 0) pagesNum = 1;
 
-		let index = 1;
-		const queueStr = `${player.queue.next.slice(0, 10).map(song => `**${index++}.** [${song.title}](${song.uri}) \`[${client.formatDuration(song.duration)}]\` • <@${!song.requester.id ? song.requester : song.requester.id}>`).join('\n')}`;
-		const queueEmbed = new Discord.MessageEmbed()
-			.setAuthor(`Queue - ${message.guild.name}`, message.guild.iconURL())
-			.setColor(client.colors.main)
-			.setDescription(`**Now Playing**: [${title}](${uri}) \`[${parsedDuration}]\` • <@${id}>.\n\n**Up Next**:\n${queueStr}`)
-			.setFooter(`Page 1/${pagesNum} | ${player.queue.length} song(s) | ${parsedQueueDuration} total duration`);
+		const songStrings = [];
+		for (let i = 0; i < player.queue.next.length; i++) {
+			const track = client.decode(player.queue.next[i].song);
+			songStrings.push(
+				`**${i + 1}.** [${track.title}](${track.uri}) \`[${client.formatDuration(track.length)}]\` • <@${player.queue.next[i].id}>
+				`);
+		}
 
-		if (player.queue.length <= 10) return message.channel.send(queueEmbed);
+		const pages = [];
+		for (let i = 0; i < pagesNum; i++) {
+			const str = songStrings.slice(i * 10, i * 10 + 10).join('');
+			const embed = new Discord.MessageEmbed()
+				.setAuthor(`Queue - ${message.guild.name}`, message.guild.iconURL())
+				.setColor(client.colors.main)
+				.setDescription(`**Now Playing**: [${title}](${uri}) \`[${parsedDuration}]\` • <@${id}>.\n\n**Up Next**:\n${str}`)
+				.setFooter(`Page ${i + 1}/${pagesNum} | ${player.queue.length} song(s) | ${parsedQueueDuration} total duration`);
+			pages.push(embed);
+		}
 
-		if (player.queue.length > 10) {
-			if(args[0]) {
-				if (isNaN(args[0])) return message.channel.send('Page must be a number.');
-				if (args[0] > pagesNum) return message.channel.send(`There are only ${pagesNum} pages available.`);
-
-				let index2 = args[0] * 10 - 10;
-				const pageStart = args[0] * 10 - 10;
-				const pageEnd = args[0] * 10;
-
-				const queueStr2 = `${player.queue.slice(pageStart, pageEnd).map(song => `**${index2++}.** [${song.title}](${song.uri}) \`[${client.formatDuration(song.duration)}]\` • <@${!song.requester.id ? song.requester : song.requester.id ? song.requester : !song.requester.id ? song.requester : song.requester.id}>`).join('\n')}`;
-				const queueEmbed2 = new Discord.MessageEmbed()
-					.setAuthor(`Queue - ${message.guild.name}`, message.guild.iconURL())
-					.setColor(client.colors.main)
-					.setDescription(`**Now Playing:** [${title}](${uri}) \`[${parsedDuration}]\` • <@${id}>.\n\n**Up Next**:\n${queueStr2}`)
-					.setFooter(`Page ${args[0]}/${pagesNum} | ${player.queue.length} song(s) • ${parsedQueueDuration} total duration`);
-				return message.channel.send(queueEmbed2);
-			}
-			else {
-				const pages = [];
-				let n = 1;
-				for (let i = 0; i < pagesNum; i++) {
-					const str = `${player.queue.slice(i * 10, i * 10 + 10).map(song => `**${n++}.** [${song.title}](${song.uri}) \`[${client.formatDuration(song.duration)}]\` • <@${!song.requester.id ? song.requester : song.requester.id}>`).join('\n')}`;
-					const embed = new Discord.MessageEmbed()
-						.setAuthor(`Queue - ${message.guild.name}`, message.guild.iconURL())
-						.setColor(client.colors.main)
-						.setDescription(`**Now Playing**: [${title}](${uri}) \`[${parsedDuration}]\` • <@${id}>.\n\n**Up Next**:\n${str}`)
-						.setFooter(`Page ${i + 1}/${pagesNum} | ${player.queue.length} song(s) | ${parsedQueueDuration} total duration`);
-					pages.push(embed);
-					if(i == pagesNum - 1) paginate(client, message, pages, ['◀️', '▶️'], 120000, player.queue.length, parsedQueueDuration);
-				}
-			}
+		if (!args[0]) {
+			if (pages.length == pagesNum && player.queue.length > 10) paginate(client, message, pages, ['◀️', '▶️'], 120000, player.queue.length, parsedQueueDuration);
+			else return message.channel.send(pages[0]);
+		}
+		else {
+			if (isNaN(args[0])) return message.channel.send('Page must be a number.');
+			if (args[0] > pagesNum) return message.channel.send(`There are only ${pagesNum} pages available.`);
+			const pageNum = args[0] == 0 ? 1 : args[0] - 1;
+			return message.channel.send(pages[pageNum]);
 		}
 	}
 };
--- src/utils/music/formatDuration.js
+++ src/utils/music/formatDuration.js
@@ -1,6 +1,7 @@
 const { Utils } = require('@tetracyl/erela.js');
 
-module.exports = (duration) => {
+module.exports = (dur) => {
+    const duration = Number(dur);
     if(isNaN(duration) || typeof duration === 'undefined') return '00:00';
     if(duration > 3600000000) return 'Live';
     return Utils.formatTime(duration, true);
