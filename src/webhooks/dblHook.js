b23727f586fed7d69ed46c88a4bb24d07995c376
Tetra <seifaziz10@gmail.com>
2020-09-01 06:11:15 -0700

    l

--- src/webhooks/dblHook.js
+++ src/webhooks/dblHook.js
@@ -6,14 +6,6 @@ const router = express.Router();
 app.use(bodyParser.json());
 
 module.exports.startUp = async (client) => {
-    app.use((req, res, next) => {
-        req.headers['Authorization'] = process.env.DBL_HOOK_PASSWORD;
-        if (!req.headers.authorization) {
-            return res.status(403).json({ error: 'Password required' });
-        }
-        next();
-    });
-
     app.listen(9836, function() {
         console.log(`[${new Date().toLocaleString()}] > [READY] DBL Hook Ready! Listening on: 9836`);
       });
@@ -23,6 +15,7 @@ module.exports.startUp = async (client) => {
 
     // eslint-disable-next-line no-unused-vars
     router.post('/discordbotlist', async (req, res) => {
+        if(req.headers.authorization || req.headers.authorization != process.env.DBL_HOOK_PASSWORD) return res.sendStatus(403);
         console.log(req.body.id);
         const id = req.body.id;
 		const user = await client.users.fetch(id);
