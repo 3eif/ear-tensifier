c6122a3a3c1164275282f6026515a1551f5e4e8b
Tetra <seifaziz10@gmail.com>
2020-09-01 05:46:54 -0700

    Update dblHook.js

--- src/webhooks/dblHook.js
+++ src/webhooks/dblHook.js
@@ -16,8 +16,8 @@ app.use((req, res, next) => {
 
 module.exports.startUp = async (client) => {
 
-    app.listen(2763, function() {
-        console.log(`[${new Date().toLocaleString()}] > [READY] DBL Hook Ready! Listening on: 34782`);
+    app.listen(4876, function() {
+        console.log(`[${new Date().toLocaleString()}] > [READY] DBL Hook Ready! Listening on: 4876`);
       });
 
     app.use(bodyParser.urlencoded({ extended: false }));
