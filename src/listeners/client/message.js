326c2492fa08607089f81b911061ac5523ed650e
Tetra <seifaziz10@gmail.com>
2020-09-16 09:09:26 -0700

    Bug fix

--- src/listeners/client/message.js
+++ src/listeners/client/message.js
@@ -138,14 +138,14 @@ module.exports = class Message extends Event {
 					b.save().catch(e => client.log(e));
 				});
 
-				const permissions = message.channel.permissionsFor(client.user);
-				if (!permissions.has('SEND_MESSAGES') || !permissions.has('READ_MESSAGE_HISTORY')) return message.author.send(`I don't have permission to read/send messages in **${message.channel.name}**!\nPlease join the support server if you need help: ${client.settings.server}`);
-				if (!permissions.has('EMBED_LINKS')) return message.channel.send(`I don't have permission to send embeds in **${message.channel.name}**!\nPlease join the support server if you need help: ${client.settings.server}`);
-
 				let cmd;
 				if (client.commands.has(command)) cmd = client.commands.get(command);
 				else if (client.aliases.has(command)) cmd = client.aliases.get(command);
 				else return;
+
+				const permissions = message.channel.permissionsFor(client.user);
+				if (!permissions.has('SEND_MESSAGES') || !permissions.has('READ_MESSAGE_HISTORY')) return message.author.send(`I don't have permission to read/send messages in **${message.channel.name}**!\nPlease join the support server if you need help: ${client.settings.server}`);
+				if (!permissions.has('EMBED_LINKS')) return message.channel.send(`I don't have permission to send embeds in **${message.channel.name}**!\nPlease join the support server if you need help: ${client.settings.server}`);
 
 				const commandName = cmd.name.toLowerCase();
 				if (process.env.NODE_ENV == 'production') Statcord.ShardingClient.postCommand(commandName, message.author.id, client);
