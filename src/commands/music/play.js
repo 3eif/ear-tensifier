8fcbc576813a74905dafbc0fab1ea021996e10d0
Seif <seifaziz10@gmail.com>
2021-03-24 07:19:45 -0700

    Update play.js

--- src/commands/music/play.js
+++ src/commands/music/play.js
@@ -60,16 +60,13 @@ module.exports = class Play extends Command {
 			}
 		}
 		else {
-			const searchQuery = {
-				source: 'soundcloud',
-				query: args.slice(0).join(' '),
-			};
-			// if (['youtube', 'soundcloud', 'bandcamp', 'mixer', 'twitch'].includes(args[0].toLowerCase())) {
-			// 	searchQuery = {
-			// 		source: args[0],
-			// 		query: args.slice(1).join(' '),
-			// 	};
-			// }
+			searchQuery = args.join(' ');
+			if (['youtube', 'soundcloud', 'bandcamp', 'mixer', 'twitch'].includes(args[0].toLowerCase())) {
+				searchQuery = {
+					source: args[0],
+					query: args.slice(1).join(' '),
+				};
+			}
 			play(client, message, msg, player, searchQuery, false);
 		}
 	}
