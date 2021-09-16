526925c95f993a5a4b19b2ddd7c02130f71f64c9
Tetracyl <seifaziz10@gmail.com>
2021-09-15 22:14:29 -0700

    Add exec and restart command

---
+++ src/commands/dev/exec.js
@@ -0,0 +1,47 @@
+const Command = require('../../structures/Command');
+
+const Discord = require('discord.js');
+const { loading } = require('../../../config/emojis.js');
+const { invisible } = require('../../../config/colors.js');
+const { post } = require('snekfetch');
+
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+
+module.exports = class Eval extends Command {
+    constructor(client) {
+        super(client, {
+            name: 'exec',
+            description: 'Runs shell or bash commands.',
+            usage: '<command>',
+            args: true,
+            permission: 'dev',
+        });
+    }
+    async run(client, message, args) {
+        const msg = await message.channel.send(`${loading} Executing command...`);
+
+        const command = args.join(' ');
+
+        try {
+            const { stdout } = await exec(command);
+            if (stdout.length < 1000) {
+                const embed = new Discord.MessageEmbed()
+                    .addField('Input', `\`\`\`bash\n${command}\`\`\``)
+                    .addField('Output', `\`\`\`bash\n${stdout}\`\`\``)
+                    .setColor(invisible);
+                msg.edit('', embed);
+            }
+            else {
+                const { body } = await post('https://www.hastebin.com/documents').send(stdout);
+                const embed = new Discord.MessageEmbed()
+                    .setTitle('Output was too long, uploaded to hastebin!')
+                    .setURL(`https://www.hastebin.com/${body.key}.js`)
+                    .setColor(invisible);
+                msg.edit('', embed);
+            }
+        } catch (e) {
+            message.channel.send(`Error \`\`\`bash\n${e}\`\`\``);
+        };
+    }
+};
---
+++ src/commands/dev/restart.js
@@ -0,0 +1,44 @@
+const Command = require('../../structures/Command');
+
+const Discord = require('discord.js');
+const { loading } = require('../../../config/emojis.js');
+const { invisible } = require('../../../config/colors.js');
+const { post } = require('snekfetch');
+
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+
+module.exports = class Eval extends Command {
+    constructor(client) {
+        super(client, {
+            name: 'restart',
+            description: 'Restarts the bot and/or lavalink.',
+            usage: '<lavalink or bot>',
+            permission: 'dev',
+        });
+    }
+    async run(client, message, args) {
+        let str = "";
+        let command = "";
+
+        if (!args[0]) {
+            str = `Restarting Ear Tensifier and Lavalink...`;
+            command = `pm2 restart EarTensifier && pm2 restart Lavalink`;
+        } else if (args[0].toLowerCase() == "lavalink") {
+            str = `Restarting Lavalink...`;
+            command = `pm2 restart Lavalink`;
+        } else if (args[0].toLowerCase() == "ear tensifier" || args[0].toLowerCase() == "ear") {
+            str = `Restarting Ear Tensifier`;
+            command = `pm2 restart EarTensifier`;
+        }
+
+        message.channel.send(str);
+
+        try {
+            await exec(command);
+        } catch (e) {
+            message.channel.send(`Error \`\`\`bash\n${e}\`\`\``);
+        };
+
+    }
+};
