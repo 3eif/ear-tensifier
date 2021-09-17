263587edfa2f2506ca147cd2b31a9c45168c526b
Tetracyl <seifaziz10@gmail.com>
2021-09-16 20:23:18 -0700

    Fix typo

--- src/player/queueEnd.js
+++ src/player/queueEnd.js
@@ -6,7 +6,7 @@ module.exports = async (client, player) => {
 	if (n == 0) {
 		msg = "Consider reviewing it **[here](https://bots.ondiscord.xyz/bots/472714545723342848/review)**.";
 	} else {
-		msg = "Consider voting for it **[here](https://top.gg/bot/472714545723342848/vote)";
+		msg = "Consider voting for it **[here](https://top.gg/bot/472714545723342848/vote)**.";
 	}
 	const embed = new Discord.MessageEmbed()
 		.setDescription('Queue ended. Enjoying Ear Tensifier? ' + msg)
