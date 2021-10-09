33398b63f2aa392b9c33576808548177c83beb96
Tetracyl <seifaziz10@gmail.com>
2021-10-09 14:10:17 -0700

    Spotify stuff

--- src/commands/music/play.js
+++ src/commands/music/play.js
@@ -28,46 +28,14 @@ module.exports = class Play extends Command {
 		if (songLimit) return msg.edit(`You have reached the **maximum** amount of songs (${songLimit} songs). Want more songs? Consider donating here: https://www.patreon.com/eartensifier`);
 
 		let searchQuery;
-		if (args[0].startsWith(client.settings.spotifyURL)) {
-			const data = await getData(args.join(' '));
-			if (data.type == 'playlist' || data.type == 'album') {
-				const sL = await client.getSongLimit(message.author.id);
-				let songsToAdd = 0;
-				if (!player.queue.length) { songsToAdd = Math.min(sL, data.tracks.items.length); }
-				else {
-					const totalSongs = player.queue.length + data.tracks.items.length;
-					if (totalSongs > sL) songsToAdd = Math.min(sL - player.queue.length, data.tracks.items.length);
-					else songsToAdd = data.tracks.items.length;
-				}
-				if (data.type == 'playlist') {
-					for (let i = 0; i < songsToAdd; i++) {
-						const song = data.tracks.items[i];
-						play(client, message, msg, player, `${song.track.name} ${song.track.artists[0].name}`, true);
-					}
-				}
-				else {
-					await data.tracks.items.forEach(song => {
-						play(client, message, msg, player, `${song.name} ${song.artists[0].name}`, true);
-					});
-				}
-				const playlistInfo = await getPreview(args.join(' '));
-				if (data.tracks.items.length != songsToAdd) msg.edit('', client.queuedEmbed(playlistInfo.title, args[0], null, songsToAdd, message.author).setFooter('You have reached the max amount of songs in the queue. Purchase premium or pro to get more.'));
-				else msg.edit('', client.queuedEmbed(playlistInfo.title, args[0], null, songsToAdd, message.author));
-			}
-			else if (data.type == 'track') {
-				const track = await getPreview(args.join(' '));
-				play(client, message, msg, player, `${track.title} ${track.artist}`, false);
-			}
+		searchQuery = args.join(' ');
+		if (['youtube', 'soundcloud', 'bandcamp', 'mixer', 'twitch'].includes(args[0].toLowerCase())) {
+			searchQuery = {
+				source: args[0],
+				query: args.slice(1).join(' '),
+			};
 		}
-		else {
-			searchQuery = args.join(' ');
-			if (['youtube', 'soundcloud', 'bandcamp', 'mixer', 'twitch'].includes(args[0].toLowerCase())) {
-				searchQuery = {
-					source: args[0],
-					query: args.slice(1).join(' '),
-				};
-			}
-			play(client, message, msg, player, searchQuery, false);
-		}
+		play(client, message, msg, player, searchQuery, false);
+
 	}
 };
--- src/player/createManager.js
+++ src/player/createManager.js
@@ -1,10 +1,19 @@
 const { Manager } = require('@tetracyl/erela.js');
+const Spotify = require("erela.js-spotify");
+
 const trackStart = require('./trackStart.js');
 const trackEnd = require('./trackEnd.js');
 const queueEnd = require('./queueEnd.js');
 
 module.exports = async (client) => {
 	client.music = new Manager({
+		plugins: [
+			new Spotify({
+				clientID: process.env.SPOTIFY_CLIENT_ID,
+				clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
+				convertUnresolved: true,
+			})
+		],
 		nodes: [{
 			host: process.env.LAVALINK_HOST,
 			port: process.env.LAVALINK_PORT,
@@ -16,17 +25,19 @@ module.exports = async (client) => {
 			if (guild) guild.shard.send(payload);
 		},
 	})
-	.on('nodeError', (node, error) => client.log(`Node error: ${error.message}`))
-	.on('queueEnd', player => {
-		queueEnd(client, player);
-	})
-	.on('trackStart', ({ textChannel }, { title, duration, author, uri }) => {
-		trackStart(client, textChannel, title, duration, author, uri);
-	})
-	.on('trackEnd', player => {
-		trackEnd(player);
-	})
-	.on('playerMove', (player, currentChannel, newChannel) => {
-		player.voiceChannel = client.channels.cache.get(newChannel);
-	});
+		.on('nodeError', (node, error) => client.log(`Node error: ${error.message}`))
+		.on('queueEnd', player => {
+			queueEnd(client, player);
+		})
+		.on('trackStart', ({ textChannel }, { title, duration, author, uri }) => {
+			trackStart(client, textChannel, title, duration, author, uri);
+		})
+		.on('trackEnd', player => {
+			trackEnd(player);
+		})
+		.on('playerMove', (player, currentChannel, newChannel) => {
+			player.voiceChannel = client.channels.cache.get(newChannel);
+		});
+
+
 };
