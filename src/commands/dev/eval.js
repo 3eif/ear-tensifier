a029ca7102266b24c1c34a79305e8f6866f69de1
Tetracyl <seifaziz10@gmail.com>
2021-10-11 20:13:02 -0700

    Closes #66

--- src/commands/dev/eval.js
+++ src/commands/dev/eval.js
@@ -2,8 +2,7 @@ const Command = require('../../structures/Command');
 
 const Discord = require('discord.js');
 const { loading } = require('../../../config/emojis.js');
-const { invisible } = require('../../../config/colors.js');
-const { inspect } = require('util');
+const util = require('util');
 const { post } = require('snekfetch');
 
 module.exports = class Eval extends Command {
@@ -19,42 +18,30 @@ module.exports = class Eval extends Command {
 	async run(client, message, args) {
 		const msg = await message.channel.send(`${loading} Executing code...`);
 
-		const code = args.join(' ');
-		let evaled = eval(code);
-		if (typeof evaled !== 'string') { evaled = require('util').inspect(evaled); }
-		if (evaled.includes(client.token)) {
-			return msg.edit('Nah fam');
-		}
+		const player = client.music.players.get(message.guild.id);
 
 		try {
-			let output = eval(code);
-			if (output instanceof Promise || (Boolean(output) && typeof output.then === 'function' && typeof output.catch === 'function')) output = await output;
-			output = inspect(output, { depth: 0, maxArrayLength: null });
-			output = clean(output);
+			let code = args[0] == '-a' ? args.slice(1).join(' ') : args.join(' ');
+			let fullCode = args[0] == '-a' ? '(async () => {\n{code}\n})()' : '{code}';
+			let str = fullCode.replace('{code}', code);
+			let output = util.inspect(await eval(str), { depth: 0 });
+
+			if (output.includes(process.env.DISCORD_TOKEN)) return msg.edit('Cannot run command since the token will be leaked.');
+
 			if (output.length < 1000) {
 				const embed = new Discord.MessageEmbed()
 					.addField('Input', `\`\`\`js\n${code}\`\`\``)
 					.addField('Output', `\`\`\`js\n${output}\`\`\``)
-					.setColor(invisible);
-				msg.edit('', embed);
-			}
-			else {
-				const { body } = await post('https://www.hastebin.com/documents').send(output);
+				return msg.edit('', embed);
+			} else {
+				const res = await post('https://hastebin.com/documents', { body: output });
 				const embed = new Discord.MessageEmbed()
 					.setTitle('Output was too long, uploaded to hastebin!')
-					.setURL(`https://www.hastebin.com/${body.key}.js`)
-					.setColor(invisible);
-				msg.edit('', embed);
+					.setURL(`https://www.hastebin.com/${res.data.key}.js`)
+				return msg.edit('', embed);
 			}
-		}
-		catch (e) {
-			message.channel.send(`Error \`\`\`js\n${e}\`\`\``);
+		} catch (e) {
+			return msg.edit(`An error occurred: \n\`\`\`js\n${e.message}\`\`\``).catch(console.error);
 		}
 	}
 };
-
-function clean(text) {
-	return text
-		.replace(/`/g, '`' + String.fromCharCode(8203))
-		.replace(/@/g, '@' + String.fromCharCode(8203));
-}
