5228b76fbc1701ffa341379abf30a282af09045e
Tetracyl <seifaziz10@gmail.com>
2021-10-23 08:27:53 -0700

    Remove extra sentry init

--- src/eartensifier.js
+++ src/eartensifier.js
@@ -4,11 +4,4 @@ const EarTensifier = require('./structures/Client');
 const client = new EarTensifier(process.env.DISCORD_TOKEN);
 client.login();
 
-if (process.env.SENTRY_URL) {
-	Sentry.init({
-		dsn: process.env.SENTRY_URL,
-		environment: process.env.SENTRY_ENVIRONMENT,
-	});
-}
-
 ['commands', 'events'].forEach(handler => require(`./handlers/${handler}`)(client));
