ffd5ce1730829f93563c737e7f97d65fbcb2d600
Tetracyl <seifaziz10@gmail.com>
2021-10-24 08:59:42 -0700

    Update channel type and clean command

--- src/commands/bot/clean.js
+++ src/commands/bot/clean.js
@@ -11,29 +11,20 @@ module.exports = class Clean extends Command {
 		});
 	}
 	async run(client, message, args) {
-		console.log('hi5');
 		if (!message.member.permissions.has(Permissions.FLAGS.MANAGE_MESSAGES)) return message.channel.send('You must have the `Manage Messages` permission to use this command.');
 
 		let messagesToDelete = 0;
 
-		console.log('hi1');
 		if (args[0]) {
-			console.log('hi2');
 			messagesToDelete = parseInt(args[0])
 			if (isNaN(messagesToDelete) || messagesToDelete < 1) {
 				return message.channel.send({ content: `Invalid argument, argument must be a number.\nCorrect Usage: \`${client.settings.prefix}clean <number messages>\`` });
 			}
 		}
 
-		if (message.channel.type == 'text') {
-			console.log('hi');
-			await message.channel.messages.fetch({ limit: 50 }).then(messages => {
-				console.log(messages);
-				let botMessages = messages.filter(msg => msg.author == message.guild.me.id).array();
-				if (messagesToDelete > 0) {
-					botMessages.splice(messagesToDelete);
-				}
-				message.channel.bulkDelete(botMessages);
+		if (message.channel.type == 'GUILD_TEXT') {
+			await message.channel.messages.fetch({ limit: 100 }).then(() => {
+				message.channel.bulkDelete(messagesToDelete || 50);
 			}).catch(err => {
 				client.log('Error while doing bulk delete');
 				client.log(err);
