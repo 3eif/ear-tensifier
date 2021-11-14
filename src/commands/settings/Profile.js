1eafb57e6472d7e86db92474cd8a2e98d88a22f2
EarTensifier <seifaziz10@gmail.com>
2021-11-14 14:38:06 -0800

    profile command

---
+++ src/commands/settings/Profile.js
@@ -0,0 +1,54 @@
+const { MessageEmbed } = require('discord.js');
+
+const Command = require('../../structures/Command');
+const User = require('../../models/User');
+
+module.exports = class Profile extends Command {
+    constructor(client) {
+        super(client, {
+            name: 'profile',
+            description: {
+                content: 'Displays a user\'s profile',
+                usage: '<user>',
+                examples: ['@user', 'user#1234'],
+            },
+        });
+    }
+    async run(client, ctx, args) {
+        await ctx.sendDeferMessage(`${client.config.emojis.typing} Fetching profile...`);
+
+        const user = ctx.ctx.mentions.members.first() || ctx.guild.members.cache.get(args[0]) || ctx.member;
+        if (!user) return ctx.editMessage('User not found');
+
+        User.findById(user.id, async (err, u) => {
+            if (err) client.log(err);
+            if (!u) {
+                const newUser = new User({ _id: user.id });
+                newUser.save().catch(e => client.log(e));
+
+                const embed = new MessageEmbed()
+                    .setThumbnail(user.user.displayAvatarURL())
+                    .addField('User', `${user.user.tag}`, true)
+                    .addField('Bio', 'No bio set')
+                    .setColor(client.config.colors.default)
+                    .setFooter('Commands Used: 0 | Songs Played: 0')
+                    .setTimestamp();
+                return ctx.editMessage({ content: null, embeds: [embed] });
+            }
+            else {
+                let bio;
+                if (!u.bio) bio = 'No bio set. To set your bio type `ear bio <desired bio>`';
+                else bio = u.bio;
+
+                const embed = new MessageEmbed()
+                    .setThumbnail(user.user.displayAvatarURL())
+                    .addField('User', `${user.user.tag}`, true)
+                    .addField('Bio', `${bio}`)
+                    .setColor(client.config.colors.default)
+                    .setFooter(`Commands Used: ${u.commandsUsed} | Songs Played: ${u.songsPlayed}`)
+                    .setTimestamp();
+                return ctx.editMessage({ content: null, embeds: [embed] });
+            }
+        });
+    }
+};
