9944ea77f6713dd89986a227d3dd1b928b18969e
EarTensifier <seifaziz10@gmail.com>
2021-12-05 11:40:29 -0800

    comment this out for now

--- src/listeners/player/TrackStart.js
+++ src/listeners/player/TrackStart.js
@@ -59,7 +59,7 @@ module.exports = class TrackStart extends Event {
             percentage = player.getTime() / duration;
 
             const e = new MessageEmbed(embed.setDescription(`${parsedCurrentDuration}  ${percentage < 0.05 ? this.client.config.emojis.progress7 : this.client.config.emojis.progress1}${this.client.config.emojis.progress2.repeat(part)}${percentage < 0.05 ? '' : this.client.config.emojis.progress3}${this.client.config.emojis.progress5.repeat(12 - part)}${this.client.config.emojis.progress6}  ${parsedDuration}`));
-            if (player.nowPlayingMessage) player.nowPlayingMessage.edit({ content: null, embeds: [e] });
+            // if (player.nowPlayingMessage) player.nowPlayingMessage.edit({ content: null, embeds: [e] });
         }, 5000);
     }
 };
