830a15b41d6023f84ded068e1a12e441d531dd50
EarTensifier <seifaziz10@gmail.com>
2021-12-07 20:41:08 -0800

    fbruh

--- src/commands/bot/Shards.js
+++ src/commands/bot/Shards.js
@@ -71,7 +71,7 @@ Memory: ${Number(i.memoryUsage).toLocaleString()} MB\nAPI: ${i.ping.toLocaleStri
 Total Servers: ${totalGuilds.toLocaleString()}\nTotal Channels: ${totalChannels.toLocaleString()}\nTotal Users: ${totalMembers.toLocaleString()}\nTotal Memory: ${totalMemory.toFixed(2)} MB\nAvg API Latency: ${avgLatency} ms\nTotal Players: ${totalPlayingPlayers}/${totalPlayers}\`\`\``);
                     embed.setTimestamp();
                     embeds.push(embed);
-                    if (embeds.length == Math.ceil(shardArray.length / 15)) {
+                    if (embeds.length == Math.ceil(shardArray.length)) {
                         await ctx.messageHelper.paginate(embeds);
                     }
                 });
