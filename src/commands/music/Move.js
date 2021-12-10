c6484915d8d954c26da0e00edf65fa1b8090d42b
EarTensifier <seifaziz10@gmail.com>
2021-12-10 07:52:57 -0800

    Fix move command

--- src/commands/music/Move.js
+++ src/commands/music/Move.js
@@ -37,7 +37,7 @@ module.exports = class Move extends Command {
         if (args[0] === 0) return ctx.sendMessage(`Cannot move a song that is already playing. To skip the current playing song type: \`${await ctx.messageHelper.getPrefix()}skip\``);
 
         const player = client.music.players.get(ctx.guild.id);
-        if ((args[0] > player.queue.length) || (args[0] && !player.queue[args[0]])) return ctx.sendMessage('Song not found.');
+        if ((args[0] > player.queue.length) || (args[0] && !player.queue[args[0] - 1])) return ctx.sendMessage('Song not found.');
 
         if (!args[1]) {
             const song = player.queue[args[0] - 1];
@@ -47,7 +47,7 @@ module.exports = class Move extends Command {
         }
         else if (args[1]) {
             if (args[1] == 0) return ctx.sendMessage(`Cannot move a song that is already playing. To skip the current playing song type: \`${await ctx.messageHelper.getPrefix()}skip\``);
-            if ((args[1] > player.queue.length) || !player.queue[args[1]]) return ctx.sendMessage('Song not found.');
+            if ((args[1] > player.queue.length) || !player.queue[args[1] - 1]) return ctx.sendMessage('Song not found.');
             const song = player.queue[args[0] - 1];
             player.queue.splice(args[0] - 1, 1);
             player.queue.splice(args[1] - 1, 0, song);
