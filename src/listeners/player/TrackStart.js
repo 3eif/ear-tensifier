5ff6d82405675066dafa60fcfbf10fa54bf5a505
Seif <seifaziz10@gmail.com>
2021-12-31 09:23:04 -0800

    Change to one min

--- src/listeners/player/TrackStart.js
+++ src/listeners/player/TrackStart.js
@@ -65,7 +65,7 @@ module.exports = class TrackStart extends Event {
 
                 const e = new MessageEmbed(embed.setDescription(`${parsedCurrentDuration}  ${percentage < 0.05 ? this.client.config.emojis.progress7 : this.client.config.emojis.progress1}${this.client.config.emojis.progress2.repeat(part)}${percentage < 0.05 ? '' : this.client.config.emojis.progress3}${this.client.config.emojis.progress5.repeat(12 - part)}${this.client.config.emojis.progress6}  ${parsedDuration}`));
                 if (player.nowPlayingMessage) player.nowPlayingMessage.edit({ content: null, embeds: [e] });
-            }, 30000);
+            }, 60000);
         }
         catch (err) {
             this.client.logger.error(err);
