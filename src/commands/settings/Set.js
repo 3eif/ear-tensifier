4da372f4c343e7ea64c6c6c0d391d6a6f4c1ba14
Seif <seifaziz10@gmail.com>
2022-03-20 09:53:38 -0700

    Fix set command

--- src/commands/settings/Set.js
+++ src/commands/settings/Set.js
@@ -12,7 +12,7 @@ module.exports = class Set extends Command {
             },
             args: true,
             permissions: {
-                userPermissions: ['MANAGE_SERVER'],
+                userPermissions: ['MANAGE_GUILD'],
             },
             options: [
                 {
