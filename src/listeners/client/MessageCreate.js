a669d89fb22f3bb835fd701379644a3ff997c20c
Seif <seifaziz10@gmail.com>
2022-09-06 07:54:51 -0700

    Bug fix

--- src/listeners/client/MessageCreate.js
+++ src/listeners/client/MessageCreate.js
@@ -53,6 +53,11 @@ module.exports = class MessageCreate extends Event {
             command = args.shift().toLowerCase();
             command = command.slice(messageHelper.server.prefix.length);
         }
+        else if (prefix === mentionPrefix) {
+            args = messageContent.split(`<@${this.client.user.id}>`);
+            args.shift();
+            command = args.shift().toLowerCase().replaceAll(' ', '');
+        }
         else {
             args = messageContent.split(' ');
             args.shift();
